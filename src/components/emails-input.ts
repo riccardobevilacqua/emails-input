d64ad5f5445d219710649078aea46cda02fe1948
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-27 15:53:39 +0200

    Added emails input component

---
+++ src/components/emails-input.ts
@@ -0,0 +1,5 @@
+export const createEmailsInput = (inputContainerNode: Element): void => {
+  inputContainerNode.innerHTML = `
+    <div>Example</div>
+  `;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import { createEmailsInput } from './components/emails-input';
 import './styles/styles.scss';
+
+const inputContainerNode = document.querySelector('#emails-input');
+
+createEmailsInput(inputContainerNode);
