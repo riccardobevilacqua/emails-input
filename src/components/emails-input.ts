fe5a578f5ad91ffd071ae6cf397cd599e17ec929
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-04-27 16:51:56 +0200

    Added buttons

--- src/components/emails-input.ts
+++ src/components/emails-input.ts
@@ -1,19 +1,23 @@
 export interface EmailsInputProps {
   title?: string;
+  baseClass?: string;
 }
 
 export const EmailsInput = (
   inputContainerNode: Element,
   {
-    title = 'Share <strong>Board name</strong> with others'
+    title = 'Share <strong>Board name</strong> with others',
+    baseClass = 'emails-input'
   }: EmailsInputProps
 ): void => {
   inputContainerNode.innerHTML = `
-    <div class="emails-input">
-      <div class="emails-input-content">
-        <div class="emails-input-title">${title}</div>
+    <div class="${baseClass}">
+      <div class="${baseClass}-content">
+        <div class="${baseClass}-title">${title}</div>
       </div>
-      <div class="emails-input-controls">
+      <div class="${baseClass}-controls">
+        <button class="${baseClass}-button">Add email</button>
+        <button class="${baseClass}-button">Get emails count</button>
       </div>
     </div>
   `;
